dd746d6a22b5d0f320fe26201671ac5c3923e3d7
RyuCroM <crom9991@nate.com>
2020-10-10 18:22:01 +0900

    문제 추가

--- src/main/scala/Lesson3/PermMissingElem.scala
+++ src/main/scala/Lesson3/PermMissingElem.scala
@@ -21,24 +21,34 @@ the elements of A are all distinct;
 each element of array A is an integer within the range [1..(N + 1)].
  */
 class PermMissingElem {
-  def sortFunc(arr: Array[Int]): Array[Int] = {
-    arr.sortWith( _ < _)
-  }
-  def solution(a: Array[Int]): Int = {
-    if( a.size < 2) a(0)+1
-    else {
-      val loop = new Breaks
-      val sortArr = sortFunc(a)
-      var rst = 0
-      loop.breakable {
-        for (i <- 0 to sortArr.size) {
-          if (sortArr(i+1) - sortArr(i) != 1 ) {
-            rst = sortArr(i) + 1
-            loop.break
-          }
-        }
-      }
-      rst
+//  def sortFunc(arr: Array[Int]): Array[Int] = {
+//    arr.sortWith( _ < _)
+//  }
+//  def solution(a: Array[Int]): Int = {
+//    if( a.size < 2) a(0)+1
+//    else {
+//      val loop = new Breaks
+//      val sortArr = sortFunc(a)
+//      var rst = 0
+//      loop.breakable {
+//        for (i <- 0 to sortArr.size) {
+//          if (sortArr(i+1) - sortArr(i) != 1 ) {
+//            rst = sortArr(i) + 1
+//            loop.break
+//          }
+//        }
+//      }
+//      rst
+//    }
+//  }
+
+  def solution(A: Array[Int]): Int = {
+    def findMissing(i: Int, L: List[Int]): Int = {
+      if (L.isEmpty || L.head != i + 1) i + 1
+      else findMissing(i + 1, L.tail)
     }
+
+    if (A.length == 0) 1
+    else findMissing(0, A.toList.sorted)
   }
 }
